e0ceb637a9c75a5b29370b0b80e27370c8a43702
mariselvam.d <mariselvam.d@nortonlifelock.com>
2022-09-18 20:05:39 +0530

    DesignPatterns repo initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void TestApplication() {
+        assertEquals(1,1);
+    }
+}
